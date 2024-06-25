894e2a840cee51ce3303e8b923ac362451ad6d16
pzzyf <2279948211@qq.com>
2024-06-25 23:03:52 +0800

    feat

--- bin/cli.js
+++ bin/cli.js
@@ -13,16 +13,25 @@ program.option("-v, --version").action(() => {
 
 // 通过 inquirer 进行问答，设置 create 命令开始创建模板
 program
-  .command("create")
+  .command("create [projectName]") // 增加可选命令 [] 表示可选
   .description("创建模版")
-  .action(async () => {
-    // 命名项目
-    const { projectName } = await inquirer.prompt({
-      type: "input",
-      name: "projectName",
-      message: "请输入项目名称：",
-    });
-      
+  .action(async (projectName) => {
+    // 命名项目，如果没有带参数则进行输入
+    if (!projectName) {
+      const { name } = await inquirer.prompt({
+        type: "input",
+        name: "projectName",
+        message: "请输入项目名称：",
+        validate: (input) => {
+          if (!input) {
+            return "项目名称不能为空";
+          }
+          return true;
+        },
+      });
+      projectName = name;
+    }
+
     // 读取 templates 目录下的所有子文件夹名字和路径
     const templatesPath = path.join(__dirname, "..", "templates"); // 注意这里的路径计算
     const files = fs.readdirSync(templatesPath, { withFileTypes: true });
@@ -60,12 +69,32 @@ program
 
     // 当全部内容选择完时，创建项目文件夹并复制模板文件到当前目录
     const targetPath = path.join(process.cwd(), projectName);
+
+    if (fs.existsSync(targetPath)) {
+      const { exist } = await inquirer.prompt({
+        type: "confirm",
+        name: "exist",
+        message: "目录已存在，是否覆盖？",
+      });
+      // 如果覆盖就递归删除文件夹继续往下执行，否的话就退出进程
+      if (exist) {
+        await fs.rm(targetPath, { recursive: true, force: true });
+      } else {
+        process.exit(1);
+      }
+    }
+
     await mkdir(targetPath);
     const selectedTemplateIndex = folderNames.indexOf(template);
     const selectedTemplatePath = folderPaths[selectedTemplateIndex];
     await copyTemplateFiles(selectedTemplatePath, targetPath);
-    console.log("模板复制完成！");
+    console.log("模板创建成功！");
+    console.log(`\ncd ${projectName}`);
+    console.log("pnpm");
+    console.log("pnpm dev\n");
   });
+
+program.on("--help", () => {});
 
 // 解析用户执行命令传入参数
 program.parse(process.argv);
